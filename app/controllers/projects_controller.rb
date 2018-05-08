0a84b8b75351598f4be79a76e32deb4ee5ecadb1
Warren <faaigame@gmail.com>
2018-05-08 00:58:28 -0700

    Add edit & destroy project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,23 @@ class ProjectsController < ApplicationController
     def show
     end
 
+    def edit
+
+    end
+
+    def update
+    	if @project.update project_params
+    		redirect_to @project, notice: "Good Job Master! I updated your project!"
+    	else
+    		render 'edit'
+    	end
+    end
+
+    def destroy
+    	@project.destroy
+    	redirect_to projects_path
+    end
+
     private
 
     def find_project
