aedbc7a1425770eeaaad014941ec665cb6870f7c
kawochan <warren.kw.chan@alumni.ubc.ca>
2018-05-12 00:06:54 -0700

    little change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'https://stark-tundra-59308.herokuapp.com' }
+  config.action_mailer.default_url_options = {host: 'https://guarded-ravine-70540.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
 
